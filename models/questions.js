83e7daa2d2374b45e55cd045357d0b386b6dfa28
Luis Amez <luis.amez.vitoria@gmail.com>
2018-11-15 15:40:24 +0000

    Add some dummy questions for development purpose

---
+++ models/questions.js
@@ -0,0 +1,14 @@
+const Questions = [
+  {
+    question: 'What is the past tense of go?',
+    options: ['goed', 'went', 'goes', 'goesed'],
+    correctAnswer: 'went'
+  },
+  {
+    question: 'What is the past tense of eat?',
+    options: ['ate', 'eated', 'eaten', 'potato'],
+    correctAnswer: 'ate'
+  }
+];
+
+module.exports = Questions;
