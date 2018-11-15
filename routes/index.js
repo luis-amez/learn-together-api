7ceea2299e6f7e31996d55ace1c6ff128651932c
Luis Amez <luis.amez.vitoria@gmail.com>
2018-11-15 15:51:12 +0000

    Add GET /question route that returns a single questions with options and correct answer

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const Questions = require('../models/questions');
+
 /* GET default resource */
-router.get('/', (req, res, next) => {
-  res.status(200).json({resource: 'Soon we will send something nice!'});
+router.get('/question', (req, res, next) => {
+  const index = Math.floor(Math.random() * Questions.length);
+  const question = Questions[index];
+
+  res.status(200).json(question);
 });
 
 module.exports = router;
