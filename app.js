66cfc7d2637566fc532f2685aeb1514a6eea5764
Luis Amez <luis.amez.vitoria@gmail.com>
2018-11-16 12:38:37 +0000

    Add CORS configuration so our front-end (at localhost:4200 for now) can make calls to the API

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require('express');
 // const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 
 const app = express();
+
+app.use(cors({
+  credentials: true,
+  origin: ['http://localhost:4200']
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
