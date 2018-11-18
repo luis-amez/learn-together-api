53929009ce3182dd7fab4efbb25350e10bfbaf53
Luis Amez <luis.amez.vitoria@gmail.com>
2018-11-18 13:31:04 +0000

    Add chirp model

---
+++ models/chirp.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
+
+const chirpSchema = new Schema({
+  content: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
+const Chirp = mongoose.model('Chirp', chirpSchema);
+
+module.exports = Chirp;
