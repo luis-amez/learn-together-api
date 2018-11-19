f3708bf471254379aec6fdce857f5068db0a43f4
Liudvikas Taluntis <liudvikast@gmail.com>
2018-11-19 12:36:35 +0000

    Added an explonation property to each question. It contains an information about why the correct answer is correct.

--- models/questions.js
+++ models/questions.js
@@ -2,22 +2,26 @@ const Questions = [
   {
     question: 'What is the past tense of go?',
     options: ['goed', 'went', 'goes', 'goesed'],
-    correctAnswer: 'went'
+    correctAnswer: 'went',
+    explonation:'big string 1'
   },
   {
     question: 'What is the past tense of eat?',
     options: ['ate', 'eated', 'eaten', 'potato'],
-    correctAnswer: 'ate'
+    correctAnswer: 'ate',
+    explonation:'big string 2'
   },
   {
     question: 'What is the past tense of work?',
     options: ['wark', 'worked', 'fizz', 'buzz'],
-    correctAnswer: 'worked'
+    correctAnswer: 'worked',
+    explonation:'big string 3'
   },
   {
     question: 'What is the past tense of drink?',
     options: ['milkshake', 'drunk', 'drinked', 'drank'],
-    correctAnswer: 'drank'
+    correctAnswer: 'drank',
+    explonation:'big string 4'
   }
 ];
 
