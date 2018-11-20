6e4463ec89f07379acfaf7b9f315ff6c6f2e239b
Liudvikas Taluntis <liudvikast@gmail.com>
2018-11-20 13:53:27 +0000

    Improved question selection. Made it so it wouldn't give repeated questions in the same array.

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,28 @@ const Questions = require('../models/questions');
 /* GET default resource */
 router.get('/question', (req, res, next) => {
   let questions = [];
+  let previouslySelectedIndexes = [];
   // TO-DO: improve questions selection and move it to the model
   while (questions.length < 2) {
     let index = Math.floor(Math.random() * Questions.length);
-    questions.push(Questions[index]);
+
+    if(!checkForRepeats(previouslySelectedIndexes, index)) {
+      questions.push(Questions[index]);
+      previouslySelectedIndexes.push(index);
+    }
   }
 
   res.status(200).json(questions);
 });
+
+function checkForRepeats(aPrevSelectIndexes, currentIndex) {
+  let isRepeated = false;
+  for(let repeatIndex of aPrevSelectIndexes){
+    if(repeatIndex == currentIndex){
+      isRepeated = true;
+    }
+  }
+  return isRepeated;
+}
 
 module.exports = router;
