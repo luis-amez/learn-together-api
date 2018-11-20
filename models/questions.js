00249d23b9a10c024f787f1031a30d3ccb187c46
Liudvikas Taluntis <liudvikast@gmail.com>
2018-11-20 14:55:55 +0000

    Added gifImage attribute to question objects, so they could hold URL's to the corresponding gif. Also added another question just to have more.

--- models/questions.js
+++ models/questions.js
@@ -3,25 +3,36 @@ const Questions = [
     question: 'What is the past tense of go?',
     options: ['goed', 'went', 'goes', 'goesed'],
     correctAnswer: 'went',
-    explonation:'big string 1'
+    explonation:'big string 1',
+    gifImage:'https://media.giphy.com/media/7E2wW2oLv3isnl6F9h/giphy.gif'
   },
   {
     question: 'What is the past tense of eat?',
     options: ['ate', 'eated', 'eaten', 'potato'],
     correctAnswer: 'ate',
-    explonation:'big string 2'
+    explonation:'big string 2',
+    gifImage:'https://media.giphy.com/media/CKsIi206Ntz4k/giphy.gif'
   },
   {
     question: 'What is the past tense of work?',
     options: ['wark', 'worked', 'fizz', 'buzz'],
     correctAnswer: 'worked',
-    explonation:'big string 3'
+    explonation:'big string 3',
+    gifImage:'https://media.giphy.com/media/xFpRLtEyXFO8hfMD9T/giphy.gif'
   },
   {
     question: 'What is the past tense of drink?',
     options: ['milkshake', 'drunk', 'drinked', 'drank'],
     correctAnswer: 'drank',
-    explonation:'big string 4'
+    explonation:'big string 4',
+    gifImage:'https://media.giphy.com/media/hllwtNe9cnAeA/giphy.gif'
+  }
+  {
+    question: 'What is the past tense of die?',
+    options: ['dye', 'dyed', 'died', 'deathed'],
+    correctAnswer: 'died',
+    explonation:'big string 5',
+    gifImage:'https://media.giphy.com/media/pPzjpxJXa0pna/giphy.gif'
   }
 ];
 
