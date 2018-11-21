b94c754e9968d327ade1769b3d4b96a66c8ab633
Liudvikas Taluntis <liudvikast@gmail.com>
2018-11-21 00:56:12 +0000

    Improved questions. Also Hacked my way through adding a newline between an image and the question.

--- models/questions.js
+++ models/questions.js
@@ -1,78 +1,65 @@
 const Questions = [
   {
-    question: 'What is the past tense of go?',
-    options: ['goed', 'went', 'goes', 'goesed'],
-    correctAnswer: 'went',
-    explonation:'big string 1',
-    gifImage:'https://media.giphy.com/media/7E2wW2oLv3isnl6F9h/giphy.gif'
+    question: "<br>Power Generator broke down. You have a back up one. How would you ask, if it works?",
+    bigQuestion: "Does our emergency generator ______ any power?",
+    options: ['blow up', 'generates', 'goes', 'generate'],
+    correctAnswer: 'generate',
+    explonation:'Correct answer is "generates" because... [this is really elaborate and amazing explonation with insight into the meaning of life itself.]',
+    gifImage:'https://media.giphy.com/media/lM86pZcDxfx5e/giphy.gif'
   },
   {
-    question: 'What is the past tense of eat?',
-    options: ['ate', 'eated', 'eaten', 'potato'],
-    correctAnswer: 'ate',
-    explonation:'big string 2',
+    question: '<br>Timmy used to be a smoker and he wants to say that he quit. How would he say it?',
+    bigQuestion: "I Quit _____ 3 months ago.",
+    options: ['smoking', 'smoked fish', 'smoke', 'being chimney'],
+    correctAnswer: 'smoking',
+    explonation:'Correct answer is "smoking" because of the grammar. Grammar formalities is not my strong point.',
     gifImage:'https://media.giphy.com/media/CKsIi206Ntz4k/giphy.gif'
   },
   {
-    question: 'What is the past tense of work?',
-    options: ['wark', 'worked', 'fizz', 'buzz'],
-    correctAnswer: 'worked',
-    explonation:'big string 3',
-    gifImage:'https://media.giphy.com/media/xFpRLtEyXFO8hfMD9T/giphy.gif'
+    question: '<br>Someone uneducated in the same room as you, claims to have proven Riemann Hypothesis, and now tries to sell it to John. You interupt it by saying:',
+    bigQuestion: "This is _____ !!! He doesn't even understand what he's talking about!",
+    options: ['an elephant', 'a hoax', 'amazing', 'a hero'],
+    correctAnswer: 'a hoax',
+    explonation:'The correct answer is "a Hoax" because the seller is trying to trick John into believing that he is buying a real proof.',
+    gifImage:'https://media.giphy.com/media/CcmdEYnRGAtNe/giphy.gif'
   },
   {
-    question: 'What is the past tense of drink?',
-    options: ['milkshake', 'drunk', 'drinked', 'drank'],
-    correctAnswer: 'drank',
-    explonation:'big string 4',
+    question: '<br>You realized that you and your friend are going to be late to a concert if you miss the bus that is due in 5min. How would you notify your friend about it?',
+    bigQuestion: "Let's go, go, go! We need to _____ to the bus station now!",
+    options: ['milkshake', 'ran', 'walk', 'run'],
+    correctAnswer: 'run',
+    explonation:'Correct answer is "run" because walking is too slow, and "ran" is past tense. So you use "run" because it is present tense',
     gifImage:'https://media.giphy.com/media/hllwtNe9cnAeA/giphy.gif'
   },
-  {
-    question: 'What is the past tense of die?',
-    options: ['dye', 'dyed', 'died', 'deathed'],
-    correctAnswer: 'died',
-    explonation:'big string 5',
-    gifImage:'https://media.giphy.com/media/yuI7fL5cR1YeA/giphy.gif'
-  },
   {////
-    question: `‘I saw a great film’
-
-    The first sentence is in the past tense. Fill the gap in the second sentence to convert the first sentence into present perfect simple tense.
-    <br/>
-    <br>
-    ‘I _____ a great film’`,
+    question: `<br>‘I saw a great film’   This sentence is in the past tense. Fill the gap in the second sentence to convert the first sentence into present perfect simple tense.`,
+    bigQuestion: "I _____ a great film",
     options: ['Am seeing', 'Will see', 'Have seen', 'Could see'],
     correctAnswer: 'Have seen',
     explonation:'The correct answer is have seen. This is because ‘have seen’ is in present perfect simple tense. The other options, though grammatically correct, are not in the tense required.',
     gifImage:'https://media.giphy.com/media/G0lXg7mTDf4MU/giphy.gif'
   },
   {
-    question: `Fill the gap with a suitable question.
-    <br/>
-    <br>
-    ‘There wasn’t enough paper in the printer, ______ ?’`,
+    question: `<br>Fill the gap with a suitable question.`,
+    bigQuestion: "There wasn’t enough paper in the printer, ______ ?",
     options: ['Are there', 'Is there', 'Was there', 'Were there'],
     correctAnswer: 'Was there',
     explonation:'Note that the statement is in the past tense and the question should be in the past tense as well. Moreover, ‘paper’ is singular, and the correct verb is ‘was’. It is not ‘were’ as this is used for plural.',
     gifImage:'https://media.giphy.com/media/TxIwSAVQL18WI/giphy.gif'
   },
   {
-    question: `Fill the gap by using the appropriate preposition.
-    <br/>
-    <br>
-    ‘No one was allowed to use any written materials _____ the exam.’`,
+    question: `<br>Fill the gap by using the appropriate preposition.`,
+    bigQuestion:"No one was allowed to use any written materials _____ the exam.",
     options: ['On', 'Bye', 'Over', 'During'],
     correctAnswer: 'During',
     explonation:'Here, we want a preposition in relation to time, therefore ‘during’ is the appropriate word. The other words are not appropriate with reference to the context of this sentence.',
     gifImage:'https://media.giphy.com/media/IPbS5R4fSUl5S/giphy.gif'
   },
   {
-    question: `Fill the gap with the appropriate word from the following options.
-    <br/>
-    <br>
-    ‘I intend to _____ to my boss about my salary.’`,
-    options: ['Tell', 'Speak', 'Talk', 'Both options 2 and 3 are correct but option 1 is wrong'],
-    correctAnswer: 'Both options 2 and 3 are correct but option 1 is wrong',
+    question: `<br>Fill the gap with the appropriate word from the following options.`,
+    bigQuestion: "I intend to _____ to my boss about my salary.",
+    options: ['Tell', 'Speak', 'Talk', 'Both options 2 and 3 are correct'],
+    correctAnswer: 'Both options 2 and 3 are correct',
     explonation:'Both ‘speak’ and ‘talk’ are appropriate with reference to the context of the sentence. Option 1 is incorrect as it is the wrong usage. ‘tell’, ‘speak’, ‘talk’ and ‘say’ are very similar and we have to be careful while using these words.',
     gifImage:'https://media.giphy.com/media/5fBH6zoAQg9dHK2ttsc/giphy.gif'
   }
